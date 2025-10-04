61594ac52f772f1edc220c05637f18c42e342434
damanpreetsingh <damanpreetsingh@digitalocean.com>
2025-10-04 12:25:31 +0530

    Use Port from env var

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { serve } from "bun";
 import index from "./index.html";
 
 const server = serve({
+  port: process.env.PORT || 3000,
   routes: {
     // Serve index.html for all unmatched routes.
     "/*": index,
@@ -21,7 +22,7 @@ const server = serve({
       },
     },
 
-    "/api/hello/:name": async req => {
+    "/api/hello/:name": async (req) => {
       const name = req.params.name;
       return Response.json({
         message: `Hello, ${name}!`,
